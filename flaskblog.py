00e18c3032ce47a39ed4bb47d84b8dbe177439cd
Giiid <gideonfummey1@gmail.com>
2023-11-15 11:51:28 +0100

    Validated forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, request, redirect
 from forms import *
 
 # configuration
@@ -11,14 +11,34 @@ app.config['SECRET_KEY'] = '964565dbc835b0ed7dbefe7248cffbcf'
 def hello():
     return render_template('home.html', title='FlaskBlog')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register')
+
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    # if request.method == 'POST':
+    #     name = request.form['username']
+    #     email = request.form['email']
+    #     password = request.form['password']
+    #     confirm_password = request.form['confirm_password']
+
+    #     if password == confirm_password:
+    #         flash(f'Account created for {form.username.data}!', 'success')
+    #         return redirect(url_for('login'))
+    #     else:
+    #         flash(f'Passwords do not match!', 'error')
+    if form.validate_on_submit():
+        if form.password.data != form.confirm_password.data:
+            flash('Passwords do not match')
+        else:
+            flash(f'Account created for {form.username.data}!', 'success')
+            return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
 
 @app.route('/login')
 def login():
@@ -31,5 +51,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
