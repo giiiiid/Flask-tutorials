3af728179fc0cc092b2c711e4dd632c941c4b8b2
Giiid <gideonfummey1@gmail.com>
2023-11-17 12:02:52 +0100

    Validating username and passwords

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from .models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
@@ -8,6 +9,17 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=8)])
     confirm_password = PasswordField('Confim Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_user(self, user):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username already exists')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already exists')
+    
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -10,6 +10,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User({self.username}, {self.email})'
+
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
