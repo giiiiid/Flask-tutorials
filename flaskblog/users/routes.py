b684dec059e84e387568cfd76ef6411c46f4998d
Giiid <gideonfummey1@gmail.com>
2023-12-03 21:19:06 +0100

    password token

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -122,8 +122,8 @@ def user_profile(id):
 @users.route('/reset_password', methods=['GET','POST'])
 def request_reset_pwd_token():
     form = Request_ResetPassword_TokenForm()
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('home'))
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         flash('Check your email to reset your password','info')
